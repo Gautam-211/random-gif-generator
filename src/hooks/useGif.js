d49bd1a7ab1ad9c47bed9aa719d5af502f3b482b
Gautam Sachdeva <gautamsachdeva20@gmail.com>
2024-04-13 02:09:08 +0530

    error handling added

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import { useEffect,useState } from 'react';
 import axios from 'axios';
+import {toast} from 'react-toastify';
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
@@ -12,13 +12,28 @@ const useGif = (tag) => {
     
     async function fetchData(){
         setLoading(true);
-
+        try{
         const {data} = await axios.get(tag ? `${url}&tag=${tag}` : url ); 
         const imageSource = data.data.images.downsized_large.url;
 
-        setLoading(false);
-
         setGif(imageSource);
+        }
+        catch(error){
+            if (error.response.status === 404){
+                toast.error("Type in a reasonable tag")
+            }
+            else if (error.response.status === 429){
+                toast.error("Request limit reached! Check again after some time")
+            }
+            else{
+                toast.error("Something went wrong");
+            }
+            
+        }
+        finally{
+             setLoading(false);
+        }
+        
     }
 
     useEffect( () => {
